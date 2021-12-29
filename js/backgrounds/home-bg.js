5769da317e63c734c487ef208c363ef152e2448b
Altycon <94148532+Altycon@users.noreply.github.com>
2021-12-28 20:55:51 -0800

    Adding files correctly

---
+++ js/backgrounds/home-bg.js
@@ -0,0 +1,56 @@
+
+// This is initialized in index.js
+
+/*
+    Creating a background for the home page
+    -final goal is to use surreal numbers in some way
+    - maybe using floating partilcls for now
+*/
+
+const HomeCanvas = createCanvas();
+const HomeCtx = HomeCanvas.getContext('2d');
+let Particles = createParticleArray(HomeCanvas,1000);
+
+
+let HomeAnimationRequest;
+
+function createCanvas(){
+    const canvas = document.createElement('canvas');
+    canvas.width = document.body.getBoundingClientRect().width;
+    canvas.height = document.body.getBoundingClientRect().height;
+    canvas.style.position = 'absolute';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    canvas.style.zIndex = '-1';
+    document.body.appendChild(canvas);
+    return canvas;
+}
+
+function createParticleArray(canvas,n){
+    let arr = [];
+    for(let i = 0; i < n; i++){
+        arr.push(new Particle(canvas, Math.random() * canvas.width, Math.random()* canvas.height));
+    }
+    return arr;
+}
+
+
+
+
+function CreateHomeBackground(){
+    
+    Particles.forEach(particle => particle.radius = Math.ceil(Math.random()* 5));
+    Particles.forEach(particle => particle.draw());
+    
+    animate();
+}
+
+
+function animate(){
+
+    HomeCtx.clearRect(0,0, HomeCanvas.width, HomeCanvas.height);
+
+    Particles.forEach(particle => particle.update());
+
+    HomeAnimationRequest = window.requestAnimationFrame(animate)
+}
---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+// create intersection observer to move circles down to bottom of page
+// and as page moves down, the largest circle will expand to fill the 
+// next section
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+
+    CreateHomeBackground();
+
+
+    addDesktopListeners()
+    
+}
+
