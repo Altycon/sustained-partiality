f1804d3620dea42a54369c554bc4e233b0dadeca
Altycon <altycon1@gmail.com>
2022-01-03 04:10:38 -0800

    removed clicked listeners/added more elements

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,40 +2,25 @@
 /*
         Notes
 
-    -Should I keep the slight 3d movement listeners? The movement is clipping the circle element when opened.
+    -Should I keep the slight 3d movement listeners? The movement is clipping the circle target when opened.
         Add and remove them maybe? But I can't figure that out correctly
 */
 
 function addDesktopListeners(){
 
-    const PrimaryContentContainer = document.querySelector('.primary-section .content');
-    const CirclesContainer = document.querySelector('.circles-container');
-    const Circles = document.querySelectorAll('.circle');
-    const Circle1 = document.querySelector('.circle1');
-    const Circle2 = document.querySelector('.circle2');
+    
 
-    addCircleEventListeners(Circles);
+    addCircleEventListeners();
 }
 
 
-function addCircleEventListeners(circles){
-    circles.forEach(circle=>{
-        circle.addEventListener('click', (ev)=>{
-            ev.target.setAttribute('data-active', 'true');
-            ev.target.classList.add('open');
-            addCloseCircleEventListeners(ev.target, circles);
-        })
-    })
+function addCircleEventListeners(){
+    
 }
 
-function addCloseCircleEventListeners(element, circles){
-    document.querySelectorAll('.close-button').forEach(button =>{
-        button.addEventListener('click', (ev)=>{
-            element.setAttribute('data-active', 'false');
-            element.classList.remove('open');
-            button.removeEventListener('click', addCloseCircleEventListeners);
-        });
-    })
+function addCloseCircleEventListeners(){
+    
+    
 }
 
 function addMobileListeners(){
