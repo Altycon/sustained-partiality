9c9098dc878be522e8adf45a5ebaa4e49b0db75e
Altycon <altycon1@gmail.com>
2022-01-04 04:15:14 -0800

    adding proper circle animations

--- js/pages/home.js
+++ js/pages/home.js
@@ -15,12 +15,32 @@ function addDesktopListeners(){
 
 
 function addCircleEventListeners(){
+    const circle2 = document.querySelector('.circle2');
+    circle2.addEventListener('click', moveCirclesToCenter)
+
+    // to reverse or close (temporary)
+    const p = document.querySelector('.words p');
+    p.addEventListener('click', moveCirclesBack)
+
+}
+
+function moveCirclesToCenter(){
+    document.querySelectorAll('.circle').forEach( circle => {
+        circle.classList.add('moveToCenter');
+    })
     
 }
 
-function addCloseCircleEventListeners(){
-    
-    
+function moveCirclesBack(){
+    document.querySelectorAll('.circle').forEach( circle => {
+        if(!circle.classList.contains('moveToCenter')){
+            return;
+        }else{
+            circle.classList.remove('moveToCenter');
+            circle.classList.add('moveBack');
+            circle.addEventListener('animationend', (ev) => {circle.classList.remove('moveBack')});
+        }
+    })
 }
 
 function addMobileListeners(){
